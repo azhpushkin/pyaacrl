636db7deba78223554d6515b5a60799f1002c430
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2021-11-20 01:24:45 +0200

    Update Cython code according to new version

--- pyaacrl/cython_src/lib.pxd
+++ pyaacrl/cython_src/lib.pxd
@@ -1,27 +1,37 @@
 from libcpp.string cimport string
+from libcpp.vector cimport vector
+from libcpp.utility cimport pair
 from libcpp.map cimport map
 
+# NOTE: this .pxd does not define all the fields and methods,
+#       but only the ones that are user from the Cython code.
+# This makes our life much easier :D
 
 cdef extern from "yaacrl/yaacrl.h" namespace "yaacrl":
     cdef cppclass CppWAVFile "yaacrl::WAVFile":
         string name
         CppWAVFile (string path)
-        CppWAVFile (string path, string name)
 
     cdef cppclass CppMP3File "yaacrl::MP3File":
         string name
         CppMP3File (string path)
-        CppMP3File (string path, string name)
 
     cdef cppclass CppFingerprint "yaacrl::Fingerprint":
         CppFingerprint(CppWAVFile file)
+
+    cdef cppclass CppStoredSong "yaacrl::StoredSong":
+        int id
         string name
     
     cdef cppclass CppStorage "yaacrl::Storage":
         CppStorage(string)
 
-        void store_fingerprint(CppFingerprint f)
-        map[string, float] get_matches(CppFingerprint f)
+        CppStoredSong store_fingerprint(CppFingerprint f, string name)
+        void delete_stored_song(CppStoredSong)
+        CppStoredSong rename_stored_song(CppStoredSong s, string new_name)
+        vector[CppStoredSong] list_songs()
+
+        vector[pair[CppStoredSong, float]] get_matches(CppFingerprint f)
 
 
 cdef extern from "yaacrl/config.h" namespace "yaacrl":
--- pyaacrl/cython_src/pyaacrl_cy.pyx
+++ pyaacrl/cython_src/pyaacrl_cy.pyx
@@ -19,33 +19,32 @@ cdef class Fingerprint:
     cdef unique_ptr[CppFingerprint] thisptr
     
     @staticmethod
-    def from_wav(path, name = None):
+    def from_wav(path):
         self = Fingerprint()
-        if name:
-            self.thisptr = make_unique[CppFingerprint](CppWAVFile(path, name))
-        else:
-            self.thisptr = make_unique[CppFingerprint](CppWAVFile(path))
-        
+        self.thisptr = make_unique[CppFingerprint](CppWAVFile(path))
         return self
 
     @staticmethod
-    def from_mp3(path, name = None):
+    def from_mp3(path):
         self = Fingerprint()
-        if name:
-            self.thisptr = make_unique[CppFingerprint](CppMP3File(path, name))
-        else:
-            self.thisptr = make_unique[CppFingerprint](CppMP3File(path))
-        
+        self.thisptr = make_unique[CppFingerprint](CppMP3File(path))
         return self
 
-    @property
-    def name(self):
-        return deref(self.thisptr).name
 
-    @name.setter
-    def name(self, new_name):
-        deref(self.thisptr).name = new_name
-            
+cdef class StoredSong:
+    cdef public int id
+    cdef public string name
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        return 'asd'
+
+
+cdef StoredSong stored_song_from_cpp(CppStoredSong cpp_song):
+    return StoredSong(id=cpp_song.id, name=cpp_song.name)
 
 
 cdef class Storage:
@@ -54,11 +53,28 @@ cdef class Storage:
     def __cinit__(self, dbfile: str):
         self.thisptr.reset(new CppStorage(dbfile))
 
-    def store_fingerprint(self, fp: Fingerprint):
-        deref(self.thisptr).store_fingerprint(deref(fp.thisptr))
+    def store_fingerprint(self, fp: Fingerprint, name: str):
+        return stored_song_from_cpp(
+            deref(self.thisptr).store_fingerprint(deref(fp.thisptr), name)
+        )
 
     def get_matches(self, fp: Fingerprint):
-        return deref(self.thisptr).get_matches(deref(fp.thisptr))
+        cpp_results = deref(self.thisptr).get_matches(deref(fp.thisptr))
+        
+        results = []
+
+        for i in range(cpp_results.size()):
+            results.append((
+                stored_song_from_cpp(cpp_results[i].first),
+                cpp_results[i].second
+            ))
+        
+        return results
+
+    def list_songs(self):
+        cpp_results = deref(self.thisptr).list_songs()
+        return [stored_song_from_cpp(song) for song in cpp_results]
+
 
 cdef void _custom_logger(CppLogLevel cpp_level, string log_msg):
     # read more about this at: http://docs.cython.org/en/latest/src/userguide/language_basics.html#error-return-values
